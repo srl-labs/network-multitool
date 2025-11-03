b286f72084c7c8c38150e06d332406a6c84c429e
jmachado <jocrismachado@gmail.com>
2025-11-03 09:16:05 +0000

    bngblaster-install: hold version in variable

--- install-bngblaster.sh
+++ install-bngblaster.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
+LIBDICT_VERSION="1.0.4"
+BNGBLASTER_VERSION="0.9.26"
 mkdir /bngblaster
 cd /bngblaster
-wget https://github.com/rtbrick/libdict/archive/refs/tags/1.0.4.zip
-unzip 1.0.4.zip
-mkdir libdict-1.0.4/build
-cd /bngblaster/libdict-1.0.4/build
+wget https://github.com/rtbrick/libdict/archive/refs/tags/$LIBDICT_VERSION.zip
+unzip $LIBDICT_VERSION.zip
+mkdir libdict-$LIBDICT_VERSION/build
+cd /bngblaster/libdict-$LIBDICT_VERSION/build
 cmake ..
 make -j16 install
 cd /bngblaster
-wget https://github.com/rtbrick/bngblaster/archive/refs/tags/0.9.26.zip
-unzip 0.9.26.zip
-mkdir bngblaster-0.9.26/build
-cd /bngblaster/bngblaster-0.9.26/build
+wget https://github.com/rtbrick/bngblaster/archive/refs/tags/$BNGBLASTER_VERSION.zip
+unzip $BNGBLASTER_VERSION.zip
+mkdir bngblaster-$BNGBLASTER_VERSION/build
+cd /bngblaster/bngblaster-$BNGBLASTER_VERSION/build
 #remove redundant include to avoid preprocessor redirect warning and consequent compilation failure
 sed -i '/#include <sys\/signal.h>/d' ../code/lwip/contrib/ports/unix/port/netif/sio.c
 #typedef for uint to avoid compilation error on alpine musl libc
@@ -22,5 +24,5 @@ sed -i '/#include <stdlib.h>/i #include <endian.h>' ../code/common/src/common.h
 find /bngblaster/bngblaster-0.9.26/code/ -type f \( -name "*.c" -o -name "*.h" \) -exec sed -i 's/\b__time_t\b/time_t/g' {} +
 #Don't error on sequence-point errors to allow build to complete on musl libc. Ideally code should be fixed on upstream repo.
 sed -i 's/APPEND CMAKE_C_FLAGS "-pthread"/APPEND CMAKE_C_FLAGS "-pthread -Wno-error=sequence-point"/' ../code/bngblaster/CMakeLists.txt
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBNGBLASTER_VERSION=0.9.26 ..
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBNGBLASTER_VERSION=$BNGBLASTER_VERSION ..
 make install
